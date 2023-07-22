3c0a156e9b81348e792d5c630aa65dc2ca20d48c
hardihood <guravgupta51@gmail.com>
2023-07-22 17:31:30 +0530

    adding page product, shop page and routing b/w them

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
-const http = require('http');
+// @ts-check
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require('path');
 
-const routes = require('./routes');
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-const server = http.createServer(routes);
+const routes = require("./routes");
 
-server.listen(3000);
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+})
+
+app.listen(3000);
+//const server = http.createServer(app);
+//WITHOUT EXPRESS USE LIKE THIS
+//const server = http.createServer(routes);
+
+//server.listen(3000);
